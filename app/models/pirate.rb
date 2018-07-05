c3761d75778c0500dcbb968c6a6f4cb13c343d2a
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-05 09:52:51 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -6,6 +6,15 @@ class Pirate
     @name = name
     @weight = weight
     @height = height
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def self.clear 
+    @@all.clear 
   end
 
 end
